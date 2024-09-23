ad6322701f916fcaf9e36895773e5026c7b109fa
Damian Jóźwiak <damian.jozwiak.lodz@gmail.com>
2024-09-23 10:18:08 +0200

    chapter7 - fix line endings

--- framework/src/DBAL/Contracts/Schema/TransactionContract.php
+++ framework/src/DBAL/Contracts/Schema/TransactionContract.php
@@ -1,12 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DJWeb\Framework\DBAL\Contracts\Schema;
 
 interface TransactionContract
 {
     public function begin(): void;
-
     public function commit(): void;
-
     public function rollback(): void;
 }
