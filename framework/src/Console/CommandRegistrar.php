d17fdfba71db1f469607b1138270b4888446587e
Damian Jóźwiak <damian.jozwiak.lodz@gmail.com>
2024-09-27 08:29:30 +0200

    chapter6 fix registering commands

--- framework/src/Console/CommandRegistrar.php
+++ framework/src/Console/CommandRegistrar.php
@@ -10,7 +10,6 @@ use ReflectionClass;
 
 class CommandRegistrar
 {
-    private static $loadedClasses = [];
     private string $commandsNamespace;
     private string $commandsDirectory;
 
@@ -28,23 +27,7 @@ class CommandRegistrar
             new RecursiveDirectoryIterator($commandsDirectory)
         );
         $files = $this->filterFiles($commandFiles);
-        $files = array_filter($files, $this->classDoesNotExist(...));
-        array_walk($files, $this->registerClassAsLoaded(...));
         array_walk($files, $this->allowOnlyCommandSubClasses(...));
-//        $files = array_filter($files, function ($file) {
-//            /** @var class-string<object> $className */
-//            $className = $this->getClassName($file);
-//            $reflectionClass = new ReflectionClass($className);
-//            return $reflectionClass->isSubclassOf(Command::class) &&
-//                ! $reflectionClass->isAbstract();
-//        });
-//
-//        foreach ($files as $file) {
-//            $className = $this->getClassName(
-//                $file,
-//            );
-//            new $className($this->app);
-//        }
     }
 
     public function allowOnlyCommandSubClasses(\SplFileInfo $file): void
@@ -55,24 +38,6 @@ class CommandRegistrar
         if ($reflectionClass->isSubclassOf(Command::class) &&
             ! $reflectionClass->isAbstract()) {
             new $className($this->app);
-        }
-    }
-
-    private function classDoesNotExist(\SplFileInfo $file): bool
-    {
-        $className = $this->getClassName($file);
-        return ! isset(self::$loadedClasses[$className]);
-    }
-
-    private function registerClassAsLoaded(\SplFileInfo $file): void
-    {
-        $className = $this->getClassName($file);
-        if (file_exists($file->getPathname())) {
-            $exists = class_exists($className, autoload: false);
-            //dump($exists);
-            if (! $exists) {
-                self::$loadedClasses[$className] = true;
-            }
         }
     }
 
@@ -106,29 +71,4 @@ class CommandRegistrar
             static fn(\SplFileInfo $file) => $file->isFile()
         );
     }
-//
-//    private function classExistsOrCanBeLoaded(
-//        string $className,
-//        \SplFileInfo $file
-//    ): bool {
-//        if (isset(self::$loadedClasses[$className])) {
-//            return self::$loadedClasses[$className];
-//        }
-//
-//        $filePath = $this->getClassFilePath($className);
-//        if (file_exists($file->getPathname())) {
-//            include_once $file->getPathname();
-//            $exists = class_exists($className);
-//            self::$loadedClasses[$className] = $exists;
-//            return $exists;
-//        }
-//
-//        self::$loadedClasses[$className] = false;
-//        return false;
-//    }
-//
-//    private function getClassFilePath(string $className): string
-//    {
-//        return str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-//    }
 }
