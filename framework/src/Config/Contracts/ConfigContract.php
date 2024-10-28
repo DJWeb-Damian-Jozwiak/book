43dfc23fa3d132461a4a9d94fb7e1c7d8cfc177d
Damian Jóźwiak <damian.jozwiak.lodz@gmail.com>
2024-10-28 14:33:12 +0100

    merge chapter 5 into chapter 6

--- framework/src/Config/Contracts/ConfigContract.php
+++ framework/src/Config/Contracts/ConfigContract.php
@@ -6,4 +6,5 @@ interface ConfigContract
 {
     public function get(string $key, mixed $default = null): mixed;
     public function set(string $key, mixed $value): void;
+    public function loadConfig(): void;
 }
