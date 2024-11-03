5c3183e9e7f4433f2cb266ff1559affb1e0a7a0c
Damian Jóźwiak <damian.jozwiak.lodz@gmail.com>
2024-11-03 11:20:21 +0100

    merge chapter 4 into chapter5

--- framework/src/Config/Contracts/ConfigContract.php
+++ framework/src/Config/Contracts/ConfigContract.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DJWeb\Framework\Config\Contracts;
 
 interface ConfigContract
 {
     public function get(string $key, mixed $default = null): mixed;
-    public function set(string $key, mixed $value): void;
+public function set(string $key, mixed $value): void;
+
 }
