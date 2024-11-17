a528514973ae1ba951e3b24a26ad2f84b930bfd8
Damian Jóźwiak <damian.jozwiak.lodz@gmail.com>
2024-11-17 12:39:11 +0100

    chapter8 register querybuilder

--- framework/src/ServiceProviders/SchemaServiceProvider.php
+++ framework/src/ServiceProviders/SchemaServiceProvider.php
@@ -8,7 +8,15 @@ use DJWeb\Framework\Container\Contracts\ContainerContract;
 use DJWeb\Framework\Container\ServiceProvider;
 use DJWeb\Framework\DBAL\Connection\MySqlConnection;
 use DJWeb\Framework\DBAL\Contracts\ConnectionContract;
+use DJWeb\Framework\DBAL\Contracts\Query\DeleteQueryBuilderContract;
+use DJWeb\Framework\DBAL\Contracts\Query\InsertQueryBuilderContract;
+use DJWeb\Framework\DBAL\Contracts\Query\SelectQueryBuilderContract;
+use DJWeb\Framework\DBAL\Contracts\Query\UpdateQueryBuilderContract;
 use DJWeb\Framework\DBAL\Contracts\Schema\SchemaContract;
+use DJWeb\Framework\DBAL\Query\Builders\DeleteQueryBuilder;
+use DJWeb\Framework\DBAL\Query\Builders\InsertQueryBuilder;
+use DJWeb\Framework\DBAL\Query\Builders\SelectQueryBuilder;
+use DJWeb\Framework\DBAL\Query\Builders\UpdateQueryBuilder;
 use DJWeb\Framework\DBAL\Schema\SchemaFactory;
 
 class SchemaServiceProvider extends ServiceProvider
@@ -21,5 +29,9 @@ class SchemaServiceProvider extends ServiceProvider
             SchemaContract::class,
             SchemaFactory::create($connection)
         );
+        $container->set(SelectQueryBuilderContract::class, new SelectQueryBuilder($connection));
+        $container->set(InsertQueryBuilderContract::class, new InsertQueryBuilder($connection));
+        $container->set(DeleteQueryBuilderContract::class, new DeleteQueryBuilder($connection));
+        $container->set(UpdateQueryBuilderContract::class, new UpdateQueryBuilder($connection));
     }
 }
