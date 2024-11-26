59610a1eff4a68a28e14fd39dbbe29beea11f2a8
Damian Jóźwiak <damian.jozwiak.lodz@gmail.com>
2024-11-26 16:15:42 +0100

    chapter 17 fix inertia

--- framework/src/View/Inertia/ResponseFactory.php
+++ framework/src/View/Inertia/ResponseFactory.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace DJWeb\Framework\View\Inertia;
 
 use DJWeb\Framework\Config\Config;
+use DJWeb\Framework\Http\JsonResponse;
 use DJWeb\Framework\Http\Response;
+use DJWeb\Framework\Http\Stream;
 use DJWeb\Framework\View\ViewManager;
 use Psr\Http\Message\ResponseInterface;
 
@@ -13,12 +15,12 @@ class ResponseFactory
 {
     public function createResponse(array $page): ResponseInterface
     {
-        $response = new Response()
-            ->withHeader('Vary', 'Accept')
+        $response = new JsonResponse($page);
+        $response = $response->withHeader('Vary', ['Accept', 'X-Requested-With'])
             ->withHeader('X-Inertia', 'true');
 
-        if ($this->isInertiaRequest()) {
-            return $response->withJson($page);
+        if ($this->isInertiaRequest() || $this->isXmlHttpRequest()) {
+            return $response;
         }
 
         $html = new ViewManager()->build(Config::get('views.default'))
@@ -26,11 +28,19 @@ class ResponseFactory
 
         return $response
             ->withHeader('Content-Type', 'text/html; charset=utf-8')
+            ->withHeader('X-Inertia', 'true')
+            ->withHeader('Vary', 'X-Inertia')
             ->withContent($html);
     }
 
     private function isInertiaRequest(): bool
     {
         return isset($_SERVER['HTTP_X_INERTIA']) && $_SERVER['HTTP_X_INERTIA'] === 'true';
+    }
+
+    private function isXmlHttpRequest(): bool
+    {
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
     }
 }
