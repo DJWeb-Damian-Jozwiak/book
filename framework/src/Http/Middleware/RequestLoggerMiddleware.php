7105da5e5d41342c08a8836605b95ede067003fe
Damian Jóźwiak <damian.jozwiak.lodz@gmail.com>
2024-11-26 16:32:57 +0100

    chapter 13 fix logger middleware

--- framework/src/Http/Middleware/RequestLoggerMiddleware.php
+++ framework/src/Http/Middleware/RequestLoggerMiddleware.php
@@ -17,8 +17,8 @@ use Throwable;
 class RequestLoggerMiddleware implements MiddlewareInterface
 {
     private const REQUEST_START_TIME = 'request_start_time';
+    private LoggerInterface $logger;
     public function __construct(
-        private LoggerInterface $logger,
         private readonly ContextBuilder $contextBuilder
     ) {
     }
